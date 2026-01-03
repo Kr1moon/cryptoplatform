829a70e775d18d9eea874d0b96d5ee5f5f742d65
[Sergey Medkov] <[medkovs070@gmail.com]>
2026-01-03 11:02:04 -1000

    implement categories and all coins table

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,27 +8,30 @@ import { cn } from "@/lib/utils"
 const Header = () => {
     const pathname = usePathname();
   return (
-    <header className="main-container inner">
+    <header>
+      <div className="main-container inner">
       <Link href='/'>
       <Image
       src='/logo.svg'
-      alt=""
+      alt="Coin logo"
       width={132}
       height={40}
       className=""
       />
+      
       </Link>
       <nav>
         <Link href='/' className={cn('nav-link', {'is-active': pathname === '/', 'is-home': true})}>
         Home
         </Link>
-        <p className="">
+        <p>
             Search Modal
         </p>
         <Link href='/coins' className={cn('nav-link', {'is-active': pathname === '/coins'})}>
         All Coins
         </Link>
       </nav>
+      </div>
     </header>
   )
 }
