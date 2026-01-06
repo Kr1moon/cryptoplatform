4b6382243a410038d8d153d151e18a76f8e678d6
[Sergey Medkov] <[medkovs070@gmail.com]>
2026-01-06 15:14:57 +0000

    last save

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
+import Categories from "@/components/home/Categories"
+import CoinOverview from "@/components/home/CoinOverview"
+import { CategoriesFallback } from "@/components/home/fallback"
+import TrendingCoins from "@/components/home/TrendingCoins"
+import { Suspense } from "react"
 
+const Page = async () => {
 
-const page = () => {
   return (
-    <p className="text-3xl text-indigo-500">
-      
-    </p>
-  )
+  <main className="main-container">
+    <section className="home-grid">
+     <Suspense fallback={<div>Loading Overview..</div>}>
+     <CoinOverview />
+     </Suspense>
+     <Suspense fallback={<div>Loading Trending...</div>}>
+     <TrendingCoins />
+     </Suspense>
+    
+    </section>
+    <section className="w-full mt-7 space-y-4">
+      <Suspense fallback={<CategoriesFallback />}>
+        <Categories />
+      </Suspense>
+    </section>
+  </main>
+  ) 
 }
 
-export default page
+export default Page
